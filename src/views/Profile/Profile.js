93855c711a1cfc9c4aaa448a14a3f6e3951c3c21
Maria Gallego <mariagestionredes@gmail.com>
2022-04-22 10:40:32 +0200

    accepted changes on profile

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { useAuthContext } from "../../contexts/AuthContext"
 
 const Profile = () => {
@@ -14,18 +14,3 @@ const Profile = () => {
 }
 
 export default Profile
-=======
-import { useAuthContext } from "../../contexts/AuthContext";
-
-const Profile = () => {
-  const { user } = useAuthContext();
-
-  return (
-    <>
-      <p>{JSON.stringify(user)}</p>
-    </>
-  )
-}
-
-export default Profile;
->>>>>>> c0cd98ea37a727a45f00776095b8392f2c299e06
