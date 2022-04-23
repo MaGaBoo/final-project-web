6b433a7189867a8749fa19e8951f2e236a59f04f
Maria Gallego <mariagestionredes@gmail.com>
2022-04-23 12:21:30 +0200

    added links to plantsList

--- src/components/PlantsList/PlantList.js
+++ src/components/PlantsList/PlantList.js
@@ -1,13 +1,14 @@
+import { Link } from 'react-router-dom';
 import plants from "../../data/plants.json";
 
 const PlantsList = () => {
   return (
     <div>
-      <ul>
-        {plants.map((plant) => {
-          return <li key={plant.id}>{plant.scientificName}</li>;
-        })}
-      </ul>
+      {plants.map((plant) => {
+        return <li>
+        <Link to={`/plants/${plant.id}`} key={plant.id}>{plant.commonName}</Link>;
+        </li>
+      })}
     </div>
   );
 };
