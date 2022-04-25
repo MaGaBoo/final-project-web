d4052cbbcbd7e5c9bea5fdf0127a1a44aa00053b
Robert Cabrera <cabrera.rch@gmail.com>
2022-04-25 20:19:00 +0200

    feat: create cart context

--- src/views/Plants/PlantDetail/PlantDetail.jsx
+++ src/views/Plants/PlantDetail/PlantDetail.jsx
@@ -1,34 +1,41 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useCartContext } from '../../../contexts/CartContext';
 import { getPlant } from '../../../services/PlantService';
 import './PlantDetail.scss';
 
 const PlantDetail = () => {
-  const [plant, setPlant] = useState({})
+  const [product, setProduct] = useState(null);
   const { id } = useParams();
+  const { addCart } = useCartContext();
 
   useEffect(() => {
     getPlant(id)
-      .then(plant => setPlant(plant))
+      .then(plant => setProduct(plant))
   },[id])
+
+  const addToCart = () => {
+    addCart(product)
+  }
 
   return (
     <>
       <h1>Plant detail</h1>
-      {plant && plant.plantCare &&
+      {product && product.plantCare &&
         <>
-        <p>Name: {plant.commonName}</p>
-        <p>Scientific name: {plant.scientificName}</p>
-        <p>Description: {plant.description}</p>
-        <p>Description: {plant.category}</p>
-        <p>Height: {plant.height}</p>
-        <img src={plant.image} alt={plant.commonName} />
-        <p>Price: {plant.price}€</p>
-        <p>Temperature: {plant.plantCare.temperature}</p>
-        <p>Light: {plant.plantCare.light}</p>
-        <p>Watering: {plant.plantCare.watering}</p>
-        <p>Difficulty: {plant.difficulty}</p>
-        <p>Pet Friendly: {plant.petFriendly}</p>
+        <p>Name: {product.commonName}</p>
+        <p>Scientific name: {product.scientificName}</p>
+        <p>Description: {product.description}</p>
+        <p>Description: {product.category}</p>
+        <p>Height: {product.height}</p>
+        <img src={product.image} alt={product.commonName} />
+        <p>Price: {product.price}€</p>
+        <p>Temperature: {product.plantCare.temperature}</p>
+        <p>Light: {product.plantCare.light}</p>
+        <p>Watering: {product.plantCare.watering}</p>
+        <p>Difficulty: {product.difficulty}</p>
+        <p>Pet Friendly: {product.petFriendly}</p>
+        <button onClick={addToCart}>Add to cart</button>
         </>
       }
     </>
