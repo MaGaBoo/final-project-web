a841a14a64d5a7f1ec79f5c67e8597225c79323a
Maria Gallego <mariagestionredes@gmail.com>
2022-04-30 10:33:51 +0200

    category views render ok

--- src/views/Categories/CactusAndSucculents/CactusAndSucculents.js
+++ src/views/Categories/CactusAndSucculents/CactusAndSucculents.js
@@ -1,11 +1,21 @@
+import { useEffect, useState } from "react";
+import { filterCactus } from "../../../services/PlantService";
+import PlantsList from "../../../components/PlantsList/PlantList";
 import "./CactusAndSucculents.scss";
 
-const CactusAndSucculents = () => {
+const Cactus = () => {
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    filterCactus().then((plants) => setPlants(plants));
+  }, []);
+
   return (
     <div>
-      <h1>🌵 🌵 🌵</h1>
+    <h1>Evergreen plants</h1>
+      <PlantsList plants={plants} />
     </div>
   );
 };
 
-export default CactusAndSucculents;
+export default Cactus;
