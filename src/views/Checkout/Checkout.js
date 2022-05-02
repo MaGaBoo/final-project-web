7c4b0290baa682a2077ba5efc5ae1a6955378c45
Maria Gallego <mariagestionredes@gmail.com>
2022-05-02 23:17:52 +0200

    first approach to scss doing checkout view

--- src/views/Checkout/Checkout.js
+++ src/views/Checkout/Checkout.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { loadStripe } from '@stripe/stripe-js';
+import React, { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
 import {
   CardElement,
   Elements,
   useStripe,
   useElements,
-} from '@stripe/react-stripe-js';
-import { payment, getCurrentUser } from '../../services/UsersService';
-import { useParams, useNavigate } from 'react-router-dom';
-import { useCartContext } from '../../contexts/CartContext';
-
+} from "@stripe/react-stripe-js";
+import { payment, getCurrentUser } from "../../services/UsersService";
+import { useParams, useNavigate } from "react-router-dom";
+import { useCartContext } from "../../contexts/CartContext";
+import "./Checkout.scss";
 
 const StripeForm = () => {
   const stripe = useStripe();
@@ -22,7 +22,7 @@ const StripeForm = () => {
   useEffect(() => {
     getCurrentUser(userId).then((user) => setUser(user));
   }, [userId]); // en onlyhacks esto está vacío
-  
+
   const totalCart = () => {
     return cartItems.reduce((acc, product) => {
       return acc + product.price;
@@ -37,49 +37,63 @@ const StripeForm = () => {
     }
 
     const { error, paymentMethod } = await stripe.createPaymentMethod({
-      type: 'card',
+      type: "card",
       card: elements.getElement(CardElement),
     });
 
     if (error) {
     } else if (paymentMethod) {
       const { id } = paymentMethod;
-      payment({ amount: totalCart(), paymentId: id, user: user.id, items: cartItems, paymentType: 'card' })
-        .then((result) => {
-          localStorage.setItem('cart', JSON.stringify([]))
-          navigate('/profile')
-        });
+      payment({
+        amount: totalCart(),
+        paymentId: id,
+        user: user.id,
+        items: cartItems,
+        paymentType: "card",
+      }).then((result) => {
+        localStorage.setItem("cart", JSON.stringify([]));
+        navigate("/profile");
+      });
     }
   };
 
-
   return (
-    <form onSubmit={handleSubmit}>
-      {cartItems &&
-        cartItems.map((cartItem) => {
-          return (
-            <div key={cartItem.id}>
-              <p>
-                <strong>{cartItem.commonName}</strong>
-              </p>
-              <p>{cartItem.price}€</p>
-              <i onClick={() => deleteProduct(cartItem.id)} className="fa-solid fa-trash"></i>
-            </div>
-          );
-        })}
-      <h2>Total: {totalCart()}€</h2>
-      <CardElement />
-      <button type='submit' disabled={!stripe || !elements}>
-        Confirm order
-      </button>
-    </form>
+    <>
+    <div className="product-headline">
+      <h3>Product</h3>
+      <h3>Price</h3>
+      </div>
+      <form onSubmit={handleSubmit}>
+        {cartItems &&
+          cartItems.map((cartItem) => {
+            return (
+              <div className="product" key={cartItem.id}>
+              <img className="product-image" src={cartItem.image} alt={cartItem.commonName} />
+                <p>
+                  <strong>{cartItem.commonName}</strong>
+                </p>
+                <p>{cartItem.price}€</p>
+                <i
+                  onClick={() => deleteProduct(cartItem.id)}
+                  className="fa-solid fa-trash"
+                ></i>
+              </div>
+            );
+          })}
+        <h2 className="total">Total: {totalCart()}€</h2>
+        <CardElement />
+        <button className="order" type="submit" disabled={!stripe || !elements}>
+          Confirm order
+        </button>
+      </form>
+    </>
   );
 };
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
 
 const CheckoutForm = () => (
-  <div className='d-flex text-center align-items-center flex-column justify-content-center'>
+  <div className="d-flex text-center align-items-center flex-column justify-content-center">
     <Elements stripe={stripePromise}>
       <StripeForm />
     </Elements>
