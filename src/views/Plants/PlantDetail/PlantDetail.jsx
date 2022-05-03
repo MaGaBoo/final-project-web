8cf7be1618357fe4806e303faa956577d414ec04
Maria Gallego <mariagestionredes@gmail.com>
2022-05-03 12:54:41 +0200

    checkout scss

--- src/views/Plants/PlantDetail/PlantDetail.jsx
+++ src/views/Plants/PlantDetail/PlantDetail.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { useAuthContext } from '../../../contexts/AuthContext';
-import { useCartContext } from '../../../contexts/CartContext';
-import { getPlant } from '../../../services/PlantService';
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useAuthContext } from "../../../contexts/AuthContext";
+import { useCartContext } from "../../../contexts/CartContext";
+import { getPlant } from "../../../services/PlantService";
 
-import './PlantDetail.scss';
+import "./PlantDetail.scss";
 
 const PlantDetail = () => {
   const [product, setProduct] = useState(null);
@@ -14,25 +14,22 @@ const PlantDetail = () => {
   const { user, getUser } = useAuthContext();
 
   useEffect(() => {
-    getPlant(id)
-      .then(plant => {
-        if (typeof (plant.plantCare) === 'string') {
-          plant.plantCare = JSON.parse(plant.plantCare)
-        }
-        setProduct(plant)
-      })
-  },[id])
-
+    getPlant(id).then((plant) => {
+      if (typeof plant.plantCare === "string") {
+        plant.plantCare = JSON.parse(plant.plantCare);
+      }
+      setProduct(plant);
+    });
+  }, [id]);
 
   const addToCart = () => {
     if (!cartItems.includes(product)) {
-      addCart(product)
-      setBuy(false)
+      addCart(product);
+      setBuy(false);
     }
-  }
+  };
 
-
-/*   useEffect(() => {
+  /*   useEffect(() => {
       cartItems && cartItems.map(item => console.log(item.id))
   })
  */
@@ -40,35 +37,42 @@ const PlantDetail = () => {
   return (
     <>
       <h1>Plant detail</h1>
-      {product && product.plantCare &&
+      {product && product.plantCare && (
         <>
-        <p>Name: {product.commonName}</p>
-        <p>Scientific name: {product.scientificName}</p>
-        <p>Description: {product.description}</p>
-        <p>Category: {product.category}</p>
-        <p>Height: {product.height}</p>
-        <img src={product.image} alt={product.commonName} />
-        <p>Price: {product.price}€</p>
-        <p>Temperature: {product.plantCare.temperature}</p>
-        <p>Light: {product.plantCare.light}</p>
-        <p>Watering: {product.plantCare.watering}</p>
-        <p>Difficulty: {product.difficulty}</p>
-        <p>Pet Friendly: {product.petFriendly ? 'Yes' : 'No'}</p>
-        <p>Owner: {product.user.name}</p>
-        <Link to={`/plant/${product.id}/edit`}>Edit plant</Link>
+          <div className="container">
+            <img className="image" src={product.image} alt={product.commonName} />
+            <div className="product-info">
+              <p>Name: {product.commonName}</p>
+              <p>Scientific name: {product.scientificName}</p>
+              <p>Description: {product.description}</p>
+              <p>Category: {product.category}</p>
+              <p>Height: {product.height}</p>
+              <p>Price: {product.price}€</p>
+              <p>Temperature: {product.plantCare.temperature}</p>
+              <p>Light: {product.plantCare.light}</p>
+              <p>Watering: {product.plantCare.watering}</p>
+              <p>Difficulty: {product.difficulty}</p>
+              <p>Pet Friendly: {product.petFriendly ? "Yes" : "No"}</p>
+              <p>Owner: {product.user.name}</p>
+              <Link to={`/plant/${product.id}/edit`}>Edit plant</Link>
+            </div>
+          </div>
         </>
-      }
+      )}
 
-      {!user ? <p>You have to log in to buy this product</p> :
+      {!user ? (
+        <p>You have to log in to buy this product</p>
+      ) : (
         <>
-
-          {user && buy ? <button onClick={addToCart}>Add to cart</button> : <p>✅ You have adopted this plant!</p> }
-
+          {user && buy ? (
+            <button onClick={addToCart}>Add to cart</button>
+          ) : (
+            <p>✅ You have adopted this plant!</p>
+          )}
         </>
-      }
-
+      )}
     </>
-  )
-}
+  );
+};
 
 export default PlantDetail;
