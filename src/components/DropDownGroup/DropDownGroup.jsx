e620fec140d746211b515953e5a8f68dd9ae34f3
Robert Cabrera <cabrera.rch@gmail.com>
2022-05-05 20:13:24 +0200

    feat: design new and edit plant

--- src/components/DropDownGroup/DropDownGroup.jsx
+++ src/components/DropDownGroup/DropDownGroup.jsx
@@ -3,9 +3,9 @@ import './DropDownGroup.scss';
 
 const DropDownGroup = ({ label, id, type, register, error, values }) => {
   return (
-    <div>
+    <div className="form-inputs">
       <label htmlFor={id}>{label}</label>
-      <select id={id} {...register(id)}>
+      <select id={id} {...register(id)} className="form-inputs__select">
         {values.map(value => {
           return (
             <option key={value} value={value}>{value}</option>
