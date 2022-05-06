ac3eecdf5ada79b2302a2c858d4a0b50bc6501d9
Robert Cabrera <cabrera.rch@gmail.com>
2022-05-06 12:29:10 +0200

    feat: added styles to menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import { Link } from 'react-router-dom';
 import { useAuthContext } from '../../contexts/AuthContext';
 import { logout } from '../../store/AccessTokenStore';
-import { useCartContext } from '../../contexts/CartContext'
+import { useCartContext } from '../../contexts/CartContext';
+import Isotipo from '../../assets/images/logo-isotipo.png'
 import './Navbar.scss';
 
 const Navbar = () => {
@@ -11,7 +12,12 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar__logo">
-        <Link to="/" className="navbar__link">Home</Link>
+        <Link to="/" className="navbar__link navbar__link--logo">
+          <div className="navbar__myplants">
+            <img src={Isotipo} alt="" className="navbar__isotipo" />
+            <span className="navbar__logotipo">Oh My Plants</span>
+          </div>
+        </Link>
       </div>
       {!user ?
         (
@@ -25,7 +31,9 @@ const Navbar = () => {
           <div className="navbar__menu">
             <Link to="/shop" className="navbar__link">Shop</Link>
             <Link to="/plant/new" className="navbar__link">Create plant</Link>
-            <Link to="/profile" className="navbar__link"><i className="fa fa-user-o navbar__user" aria-hidden="true"></i></Link>
+            <Link to="/profile" className="navbar__link">
+              <i className="fa fa-user-o navbar__user" aria-hidden="true"></i>
+            </Link>
             <Link to="/shoppingCart" className="navbar__link">
               <div className="navbar__cart">
                 <p className="navbar__cart-number">{cartItems.length}</p>
@@ -34,7 +42,7 @@ const Navbar = () => {
             </Link>
             
 
-            <button onClick={logout}>Logout</button>
+            <button onClick={logout} className="navbar__logout">Logout</button>
           </div>
         )   
       }
