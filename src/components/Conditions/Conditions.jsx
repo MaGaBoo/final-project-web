83868f033ffcbeb840639f9702d9e83672eae501
Robert Cabrera <cabrera.rch@gmail.com>
2022-05-07 09:55:22 +0200

    feat: responsive design

--- src/components/Conditions/Conditions.jsx
+++ src/components/Conditions/Conditions.jsx
@@ -3,7 +3,7 @@ import './Conditions.scss';
 const Conditions = ({ img, title, text }) => {
   return (
     <div className="condition">
-      <div className="condition__circle"></div>
+      <div className="condition__circle">{img}</div>
 {/*       <img src={img} alt={title} /> */}
       <h3>{title}</h3>
       <p>{text}</p>
